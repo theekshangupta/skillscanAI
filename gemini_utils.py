c47e31095d84460ce7e167e44663002ed8d7a811
theekshan69 <158997531+theekshan69@users.noreply.github.com>
2025-06-04 17:03:54 +0530

    Update gemini_utils.py

--- gemini_utils.py
+++ gemini_utils.py
@@ -2,24 +2,24 @@ import pdf2image
 import pytesseract
 import requests
 import re
-# import fitz  # PyMuPDF
+import fitz
 
-# def extract_text_from_pdf(file_path):
-#     text = ""
-#     with fitz.open(file_path) as doc:
-#         for page in doc:
-#             text += page.get_text()
-#     return text
+def extract_text_from_pdf(file_path):
+    text = ""
+    with fitz.open(file_path) as doc:
+        for page in doc:
+            text += page.get_text()
+    return text
 
 # Gemini API Key
 GEMINI_API_KEY = "AIzaSyDSNKVFGhfCCX6Onx5b8NEyk38qTH-YRXg"
 
-def extract_text_from_pdf(file_path):
-    images = pdf2image.convert_from_path(file_path)
-    text = ""
-    for img in images:
-        text += pytesseract.image_to_string(img)
-    return text
+# def extract_text_from_pdf(file_path):
+#     images = pdf2image.convert_from_path(file_path)
+#     text = ""
+#     for img in images:
+#         text += pytesseract.image_to_string(img)
+#     return text
 
 def clean_markdown(text: str) -> str:
     text = text.replace("*", "")
